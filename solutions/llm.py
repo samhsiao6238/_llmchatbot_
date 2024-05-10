423435ae3f00cf0813772eac73cf015e5fc3ea49
samhsiao6238 <samhsiao6238@gmail.com>
2024-05-10 18:55:26 +0000

    s999

--- solutions/llm.py
+++ solutions/llm.py
@@ -1,19 +1,20 @@
+# llm.py
 # 導入自訂函數
 from solutions.tools.secret import get_secret
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 # 載入庫
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 # 載入環境變數
-load_dotenv()
+# load_dotenv()
 
 # 註解原本取的變數的方式
 # OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # OPENAI_MODEL = os.getenv("OPENAI_MODEL")
 # 改寫
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-OPENAI_MODEL = os.getenv("OPENAI_MODEL")
+OPENAI_API_KEY = get_secret("OPENAI_API_KEY")
+OPENAI_MODEL = get_secret("OPENAI_MODEL")
 
 # 建立 ChatOpenAI 實體
 llm = ChatOpenAI(
